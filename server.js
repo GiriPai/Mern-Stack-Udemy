e9df33e8b16a7f4f6542b2e620d6003f8ceae7b4
Giri-Pai <giri@gaindecktechnologies.com>
2019-12-07 21:17:00 +0530

    Connecting to mongodb

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+//Connect Database
+connectDB();
 
 app.get("/", (req, res) => res.send("API Running"));
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
