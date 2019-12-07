412adfa28c2be45150eb035111b33a9f827670d7
Giri-Pai <giri@gaindecktechnologies.com>
2019-12-07 21:32:32 +0530

    Route files with express

--- server.js
+++ server.js
@@ -8,5 +8,11 @@ connectDB();
 
 app.get("/", (req, res) => res.send("API Running"));
 
+//Defile Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
