ec449b20460431dd3b9b89871aaa8f8c9cf6ba6b
Giri-Pai <giri@gaindecktechnologies.com>
2019-12-07 23:36:10 +0530

    Auth middlewares

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
-//@route GET api/auth
-//@desc Test route
-//@access Public
-router.get("/", (req, res) => res.send("User route"));
+const User = require("../../models/User");
+//@route  GET api/auth
+//@desc   Test route
+//@access Protected
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
