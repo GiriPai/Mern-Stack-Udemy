061f718ac0311b7e2158ce7d445e52ce6c40413a
Giri-Pai <giri@gaindecktechnologies.com>
2020-05-06 11:24:35 +0530

    before deployment

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const config = require("config");
-// const db = config.get("mongoURI");
-const db = "mongodb://localhost:27017/meandb";
+const db = config.get("mongoURI");
+// const db = "mongodb://localhost:27017/meandb";
 
 const connectDB = async () => {
   try {
@@ -9,7 +9,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
-      useFindAndModify: false
+      useFindAndModify: false,
     });
 
     console.log("MongoDB Connected...");
